3bdb8f789a5b71736e8251a4e903c7a47b32913e
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-27 11:38:01 +0200

    Delopgave 1.1

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
 public class UserInterface {
+
+    Scanner sc = new Scanner(System.in);
+
+    public void handleInput(){
+        System.out.println("What do you want to do?");
+        String action = sc.nextLine();
+
+        switch (action){
+            case "go north":
+                System.out.println("going north");
+                break;
+
+            case "go south":
+                System.out.println("going south");
+                break;
+
+            case "go east":
+                System.out.println("going east");
+                break;
+
+            case "go west":
+                System.out.println("going west");
+                break;
+
+            case "look":
+                System.out.println("looking around");
+                break;
+        }
+    }
+
 }
