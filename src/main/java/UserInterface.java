6e924cb389b3dc4db54c24ef0156ef3796de65ba
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-28 12:55:23 +0200

    Delopgave 1 - refactored better switch in UI

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -16,19 +16,19 @@ public class UserInterface {
             System.out.println("What do you want to do?");
             String action = sc.nextLine().toLowerCase();
 
-            //Alternative inputs
-            if (action.equals("n") || action.contains("north")) {
-                action = "go north";
-            } else if (action.equals("s")   || action.contains("south")) {
-                action = "go south";
-            } else if (action.equals("e") || action.contains("east")) {
-                action = "go east";
-            } else if (action.equals("w")  || action.contains("west")) {
-                action = "go west";
-            }
+//            //Alternative inputs
+//            if (action.equals("n") || action.contains("north")) {
+//                action = "go north";
+//            } else if (action.equals("s")   || action.contains("south")) {
+//                action = "go south";
+//            } else if (action.equals("e") || action.contains("east")) {
+//                action = "go east";
+//            } else if (action.equals("w")  || action.contains("west")) {
+//                action = "go west";
+//            }
 
             switch (action) {
-                case "go north":
+                case "go north", "north", "n" -> {
                     if (adventure.player.getCurrentRoom().getNorth() != null) {
                         adventure.player.setCurrentRoom(adventure.player.getCurrentRoom().getNorth());
                         System.out.println("going north");
@@ -36,9 +36,9 @@ public class UserInterface {
                     } else if (adventure.player.getCurrentRoom().getNorth() == null) {
                         System.out.println("You can't go that way!");
                     }
-                    break;
+                }
 
-                case "go south":
+                case "go south", "south", "s" -> {
                     if (adventure.player.getCurrentRoom().getSouth() != null) {
                         adventure.player.setCurrentRoom(adventure.player.getCurrentRoom().getSouth());
                         System.out.println("going south");
@@ -46,9 +46,9 @@ public class UserInterface {
                     } else if (adventure.player.getCurrentRoom().getSouth() == null) {
                         System.out.println("You can't go that way!");
                     }
-                    break;
+                }
 
-                case "go east":
+                case "go east", "east", "e" -> {
                     if (adventure.player.getCurrentRoom().getEast() != null) {
                         adventure.player.setCurrentRoom(adventure.player.getCurrentRoom().getEast());
                         System.out.println("going east");
@@ -56,9 +56,9 @@ public class UserInterface {
                     } else if (adventure.player.getCurrentRoom().getEast() == null) {
                         System.out.println("You can't go that way!");
                     }
-                    break;
+                }
 
-                case "go west":
+                case "go west", "west", "w" -> {
                     if (adventure.player.getCurrentRoom().getWest() != null) {
                         adventure.player.setCurrentRoom(adventure.player.getCurrentRoom().getWest());
                         System.out.println("going west");
@@ -66,33 +66,25 @@ public class UserInterface {
                     } else if (adventure.player.getCurrentRoom().getWest() == null) {
                         System.out.println("You can't go that way!");
                     }
-                    break;
+                }
 
-                case "look":
-                    System.out.println(adventure.player.getCurrentRoom());
-                    break;
+                case "look" -> System.out.println(adventure.player.getCurrentRoom());
+                case "help" -> System.out.println("""
+                                                
+                                                COMMANDS:
+                                                
+                                                go north
+                                                go south
+                                                go east
+                                                go west
+                                                look
+                                                exit
+                                                
+                                                """);
 
-                case "help":
-                    System.out.println("""
-                                    
-                                    COMMANDS:
-                                    
-                                    go north
-                                    go south
-                                    go east
-                                    go west
-                                    look
-                                    exit
-                                    
-                                    """);
-                    break;
 
-                case "exit" :
-                    System.exit(0);
-
-                default:
-                    System.out.println("Invalid request, try again.");
-                    break;
+                case "exit" -> System.exit(0);
+                default-> System.out.println("Invalid request, try again.");
             }
         }
     }
